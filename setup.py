8ee66cf8d196c28cb7a036157fdaf67a8bd47e6c
joosthoeks <joosthoeks@gmail.com>
2021-12-28 15:36:40 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+
+
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='ehMLlib',
+    version='20211228.0',
+    description='Machine Learning Library Time-Series',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/febuz/ehMLlib',
+    author='febuz',
+    author_email='febuz@email',
+    classifiers=[
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    ],
+    project_urls={
+    'Documentation': 'https://github.com/febuz/ehMLlib/blob/master/README.md',
+    'Funding': 'https://github.com/febuz/ehMLlib/stargazers',
+    'Say Thanks!': 'https://github.com/febuz/ehMLlib/stargazers',
+    'Source': 'https://github.com/febuz/ehMLlib',
+    'Tracker': 'https://github.com/febuz/ehMLlib/issues',
+    },
+    packages=find_packages(),
+    py_modules=[],
+    install_requires=[],
+    python_requires='>=3',
+)
