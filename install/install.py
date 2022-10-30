ddae2059dc304b5ee0dd8ea725ea7564f7c66cec
mac <mac@edwins-air.home>
2022-10-30 20:07:15 +0100

    requirements

--- install/install.py
+++ install/install.py
@@ -1,6 +1,10 @@
 # Install packages
-!pip3 install findspark
-!pip3 install py4j
-!pip3 install pyspark
-!pip3 install pyarrow
-!pip3 install fastparquet
+def install():
+    !pip3 install findspark
+    !pip3 install py4j
+    !pip3 install pyspark
+    !pip3 install pyarrow
+    !pip3 install fastparquet
+    return print('installed')
+
+install()
---
+++ main_signals.py
@@ -0,0 +1,1 @@
+import install.install as ii
